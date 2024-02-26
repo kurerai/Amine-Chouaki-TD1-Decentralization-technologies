753867511f24b27cfcc982e5bbefca8c1cc88bfc
kurerai <119663889+kurerai@users.noreply.github.com>
2024-02-26 14:46:36 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Amine-Chouaki-TD1-Decentralization-technologies',
+    version='1.0.0',
+    author='[your-full-name-or-your-github-username]',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
